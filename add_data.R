cbf69e8830ef88747a67da1e8f76dc58e0e095c5
AntonAks <antonaks84@gmail.com>
2017-01-31 23:58:03 +0200

    add facet_grid

--- add_data.R
+++ add_data.R
@@ -1,13 +1,22 @@
 library(readr)
 library(ggplot2)
+library(dplyr)
 
 data_set <- read_csv("~/R/Executions-in-the-United-States/data/database.csv")
 
 data_set$Year <-  as.integer(substr(data_set$Date,7,10))
+data_set <- data_set%>%
+  filter(`Victim Count`<50)
 
-p <- ggplot(data = data_set, aes(x = data_set$Age)) + 
-  geom_histogram()
 
-p1 <- ggplot(data = data_set, aes(x = data_set$Year, y = data_set$`Victim Count`)) + 
-  geom_point()
+attach(data_set)
+
+p <- ggplot(data = data_set, aes(x = Age)) + 
+  geom_histogram(bins=30, color="black",fill="lightblue")
+p
+
+
+p1 <- ggplot(data = data_set, aes(x = Year, y = `Victim Count`)) + 
+  geom_jitter() +
+  facet_grid(Method ~ .)
 p1
